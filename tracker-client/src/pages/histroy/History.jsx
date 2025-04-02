7cc953a2695d1402c341249b46a927f0d788dd0c
Mostafa-Al-Bilani <mostafaalbilani@gmail.com>
2025-04-02 20:53:15 +0300

    created history page

---
+++ tracker-client/src/pages/histroy/History.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./History.css";
+import { useSelector } from "react-redux";
+
+const History = () => {
+  const historyData = useSelector((state) => state.activity.history);
+
+  return (
+    <div className="history">
+      <h2 className="history-title">Activity History</h2>
+      <ul className="history-list">
+        {historyData.map((entry, index) => (
+          <li key={index} className="history-item">
+            {entry.date} - {entry.steps} steps, {entry.distance} km, {entry.activeMinutes} min
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default History;
